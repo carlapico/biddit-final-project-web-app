303dfb99d7654f716b433315dec4db3bdc4a7529
Carla Pico <carlaspico@gmail.com>
2022-09-12 14:05:51 -0400

    fixed the bug so that it posts

--- src/homefeed/cards/NewPost.jsx
+++ src/homefeed/cards/NewPost.jsx
@@ -5,58 +5,6 @@ import "./NewPost.css"
 
 export default function NewPost () {
 
-    // const [formSubmitted, setFormSubmitted] = useState(false) //this is so it doesn't submit before information is imput
-    // const [validForm, setValidForm] = useState(false) // this is to make a form valid
-    // const [errorMessage, setErrorMessage] = useState('') // if there is an error when submitting the form 
-    // const [form, setForm] = useState({}) // not 100% sure on this 
-  
-    // console.log('props here', ronsProps, aliciasProps) // these were the props brought in from the example we had // I don't think we need to bring in any props here...
-  
-    // useEffect(() => {
-    //   if (form?.title?.length > 3 && form?.description?.length > 10) { // I need more info to see what is happening here. 
-    //     setValidForm(true)
-    //   } else {
-    //     setValidForm(false)
-    //   }
-    // }, [form])
-  
-    // async function formSubmit(e) {
-    //   e.preventDefault() // stops the page refresh
-  
-    //   if (!validForm) {
-    //     setErrorMessage('Not a valid form')
-    //     return
-    //   }
-  
-    //   try {
-    //     const results = await fetch('https://biddit-final-project-cp.web.app/userPosts', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify(form),
-    //     })
-    //     console.log(results)
-    //     const data = await results.json()
-    //     console.log(data)
-  
-    //     setFormSubmitted(true)
-    //     setErrorMessage('')
-    //     setValidForm(true)
-  
-    //     // setStateFromChild(form) // this is a prop that is being pulled
-    //   } catch (error) {
-    //     console.error(error)
-    //     setErrorMessage('there was an error submitting your comment' + error.toString())
-    //   }
-    // }
-  
-    // console.log('this is form =>', form)
-  
-    // const updateForm = event => {
-    //   setForm({ ...form, [event.target.name]: event.target.value })
-    // }
-
     const [name, setName] = useState("")
     const [county, setCounty] = useState("")
     const [service, setService] = useState ("")
@@ -112,13 +60,13 @@ export default function NewPost () {
                    <section className="newCardSection">
                        <h3> Project Requirements </h3>
                        <input type="text" value ={requirement} name="projectRequirements" onChange={(e)=>setRequirement(e.target.value)}/>
-                   </section>
+                   </section> 
 
                    <section className="newCardSection">
                        <h3> Budget and Timeline </h3>
                        <div>
-                       <input type="text" value ={projBudget} name="projectRequirements" onChange={(e)=>setProjBudget(e.target.value)}/>
-                       <input type="text" value ={projTimeline} name="projectRequirements" onChange={(e)=>setProjTimeline(e.target.value)}/>
+                       <input type="text" value ={projBudget} name="projectBudget" onChange={(e)=>setProjBudget(e.target.value)}/>
+                       <input type="text" value ={projTimeline} name="projectTimeline" onChange={(e)=>setProjTimeline(e.target.value)}/>
                        </div>
                    </section>
                </div>
